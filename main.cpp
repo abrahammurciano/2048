ea1bbfc06462c1ab82ab9710f11ff35dee12c616
Abraham Murciano <abrahammurciano@gmail.com>
2018-10-16 16:35:29 +0300

    Program asks user for grid size. Corrected function declaration syntax

--- main.cpp
+++ main.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int function rotate (int times);
-int function populate (int grid);
-void function display (int grid);
-int function move (int relativeGrid);
-int function joinSquares (int currentGrid, int grid);
-int function updateGrid (int currentGrid, int grid, int relativeGrid);
-bool function gameOver (int grid);
+int rotate (int times);
+int populate (int grid);
+void display (int grid);
+int move (int relativeGrid);
+int joinSquares (int currentGrid, int grid);
+int updateGrid (int currentGrid, int grid, int relativeGrid);
+bool gameOver (int grid);
 
 int main () {
 
-	
+	int gridSize;
+	int const limit = 10;
+	cout << "Enter a grid size:" << endl;
+	cout << "Example: For a 4x4 grid, type \"4\"" << endl;
+	cin >> gridSize;
+	while (gridSize < 2 || gridSize > limit) {
+		cout << "Size must be between 2 and " << limit << endl;
+		cin >> gridSize;
+	}
 
 	return 0;
 }
 
-int function rotate (int times) {
+int rotate (int times) {
 /*
  * will put the coordinates of each square in grid into a newGrid to return, 
  * but as if newGrid was rotated 90*times degrees clockwise. 
@@ -27,14 +35,14 @@ int function rotate (int times) {
 
 }
 
-int function populate (int grid) {
+int populate (int grid) {
 /*
  * stick some new squares into grid (according to rules of the game). returns grid
  */
 	
 }
 
-void function display (int grid) {
+void display (int grid) {
 /*
  * Output the grid onto the terminal
  * (Possibly can detect size of terminal and acomodate the grid accordingly)
@@ -43,7 +51,7 @@ void function display (int grid) {
 	
 }
 
-int function move (int relativeGrid) {
+int move (int relativeGrid) {
 /*
  * moves all squares as far as it can in the direction direction.
  * Will do so by swapping the coordinates of every non-zero square in relativeGrid
@@ -54,7 +62,7 @@ int function move (int relativeGrid) {
 	
 }
 
-int function joinSquares (int currentGrid, int grid) {
+int joinSquares (int currentGrid, int grid) {
 /*
  * joins squares when moved towards a square of the same value.
  * will do so by iterating through the squares from bottom to top 
@@ -66,7 +74,7 @@ int function joinSquares (int currentGrid, int grid) {
 	
 }
 
-int function updateGrid (int currentGrid, int grid, int relativeGrid) {
+int updateGrid (int currentGrid, int grid, int relativeGrid) {
 /*
  * uses coordinates from currentGrid to get values from grid 
  * and assign them to newGrid by taking corresponding coordinates from relativeGrid.
@@ -75,7 +83,7 @@ int function updateGrid (int currentGrid, int grid, int relativeGrid) {
 	
 }
 
-bool function gameOver (int grid) {
+bool gameOver (int grid) {
 /*
  * if empty squares in grid, return false.
  * if any 2 adjacent squares in grid are the same, return false.
